dc9e3526c87926a8b930783dc6d9e9f663a56144
HellenBianchini <hellenbiangod@gmail.com>
2024-07-12 13:43:07 -0300

    Adiciona exercicio 14

--- ListaExercicios/14.js
+++ ListaExercicios/14.js
@@ -1,1 +1,9 @@
-//Considere uma situação em que você está verificando se um usuário está logado e tem permissão de administrador para acessar determinada funcionalidade. Utilize variáveis booleanas para simular essas condições e use o operador AND para verificar se ambas são verdadeiras.
+//Considere uma situação em que você está verificando se um usuário está logado e tem permissão de administrador 
+//para acessar determinada funcionalidade. Utilize variáveis booleanas para simular essas condições e use o operador AND para verificar se ambas são verdadeiras.
+const usuario = "Fulano";
+const permissao = true;
+if(usuario || permissao){
+    console.log("LOGA");
+}else{
+    console.log("NAO LOGA")
+}
