94d626683d643726dc802a346b47331af16f45a8
HellenBianchini <hellenbiangod@gmail.com>
2024-07-12 14:32:18 -0300

    Add exercicio 19

--- ListaExercicios/19.js
+++ ListaExercicios/19.js
@@ -1,1 +1,14 @@
-//Crie uma função que receba uma string e verifique se é um palíndromo (uma palavra que é lida da mesma forma de trás para frente) utilizando o método de string reverse(). Retorne true se for um palíndromo e false caso contrário. Imprima o resultado no console.
+//Crie uma função que receba uma string e verifique se é um palíndromo (uma palavra que é lida da mesma forma de trás para frente) utilizando o método de string reverse(). 
+//Retorne true se for um palíndromo e false caso contrário. Imprima o resultado no console.
+const palavra = "hellen";
+function verificaPalindromo(palavra){
+    const normalizada = palavra.toLowerCase();
+    const reverso = normalizada.split('').reverse().join('');
+    if(normalizada === reverso){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+console.log(verificaPalindromo(palavra));
