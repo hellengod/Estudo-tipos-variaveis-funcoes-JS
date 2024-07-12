14b3e3a79222f99873667a2cff95a34ae0e7d22c
HellenBianchini <hellenbiangod@gmail.com>
2024-07-12 13:03:14 -0300

    adiciona exercicio 12

--- ListaExercicios/12.js
+++ ListaExercicios/12.js
@@ -1,2 +1,8 @@
 //Crie variáveis que armazenem o valor do saldo, de depósito e de um saque de uma conta bancária, depois realize operações de adição e subtração entre eles utilizando uma variável 
 //chamada operacao, considerando a função de cada variável criada anteriormente. Exiba os resultados finais no console.
+let saldo = 1000;
+let deposito = 100;
+operacaoRecebe = saldo + deposito;
+operacaoTransfere = saldo - deposito;
+console.log(operacaoRecebe);
+console.log(operacaoTransfere);
