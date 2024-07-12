08ddafe8d7113938e4389db0a15182fefb138901
HellenBianchini <hellenbiangod@gmail.com>
2024-07-12 14:16:09 -0300

    Adiciona exercicio 16 e 17

--- ListaExercicios/16.js
+++ ListaExercicios/16.js
@@ -1,1 +1,10 @@
-//Imagine um sistema que determina se um usuário tem idade suficiente para comprar o ingresso para um show. Declare duas variáveis que determinem a idade mínima e qual a idade do usuário e utilize estruturas condicionais (if, else) para determinar se o usuário pode realizar a compra.
+//Imagine um sistema que determina se um usuário tem idade suficiente para comprar o ingresso para um show. 
+//Declare duas variáveis que determinem a idade mínima e qual a idade do usuário e utilize estruturas condicionais (if, else) para determinar se o usuário pode realizar a compra.
+const idadeMinima = 18;
+const idadeComprador = 19;
+if(idadeComprador < idadeMinima){
+    console.log("E NECESSARIO TER 18 ANOS PARA REALIZAR A COMPRA")
+}else{
+    console.log("COMPRA REALIZADA COM SUCESSO")
+
+}
--- ListaExercicios/17.js
+++ ListaExercicios/17.js
@@ -1,1 +1,7 @@
 //Crie uma função que receba o nome de uma pessoa como argumento e retorne uma saudação personalizada. Em seguida, chame a função e exiba a saudação no console.
+const nome = "Hellen";
+function saudacaoPersonalizada(nome){
+    saudacao = `Ola ${nome} como voce esta?`;
+    return saudacao;
+}
+console.log(saudacaoPersonalizada(nome));
