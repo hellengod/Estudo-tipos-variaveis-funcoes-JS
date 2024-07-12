62fb51f95d95dfe1255cfdf37c4f6d812b475571
HellenBianchini <hellenbiangod@gmail.com>
2024-07-12 14:24:21 -0300

    Adiciona exercicio 18

--- ListaExercicios/18.js
+++ ListaExercicios/18.js
@@ -1,1 +1,12 @@
 //Crie uma função que receba a idade de uma pessoa e retorne se ela é maior de idade (idade >= 18). Imprima o resultado no console.
+const idade = 18;
+function verificaIdade(idade){
+    if(idade >= 18){
+        let verifica = "maior de idade"
+        return verifica;
+    }else{
+        verifica = "menor de idade"
+        return verifica;
+    }
+}
+console.log(verificaIdade(idade));
