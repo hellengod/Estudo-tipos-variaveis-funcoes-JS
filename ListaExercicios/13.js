b4218a75df297cf17711f0308a83997b27f7727f
HellenBianchini <hellenbiangod@gmail.com>
2024-07-12 13:12:58 -0300

    adiciona exercicio 13

--- ListaExercicios/13.js
+++ ListaExercicios/13.js
@@ -1,2 +1,5 @@
 //Crie uma variável numérica com um valor. Utilize um operador ternário para verificar se esse valor é par ou ímpar. Exiba o resultado no console.
 
+const numero = 5;
+const verifica = (numero % 2 === 0) ? "E par" : "Nao e par";
+console.log("A variável " + numero + " " + verifica);
