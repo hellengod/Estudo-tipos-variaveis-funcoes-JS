d50298b7d4fc8eefe60f9ad3b0ad8308b00f0ba7
HellenBianchini <hellenbiangod@gmail.com>
2024-07-16 19:33:01 -0300

    Adiciona enunciado 20 a 26

--- ListaExercicios/21.js
+++ ListaExercicios/21.js
@@ -1,1 +1,2 @@
-//Crie uma arrow function chamada calculaPotencia que receba dois parâmetros: a base e o expoente. A função deve calcular a potência da base elevada ao expoente e retornar o resultado.
+//Crie uma arrow function chamada calculaPotencia que receba dois parâmetros: a base e o expoente.
+// A função deve calcular a potência da base elevada ao expoente e retornar o resultado.
---
+++ ListaExercicios/23.js
@@ -0,0 +1,1 @@
+//Crie um programa em Node.js que inicie com um array cinco elementos e atribua um novo valor para a primeira posição (índice 0) desse array. Em seguida, exiba o array antes e depois da alteração.
